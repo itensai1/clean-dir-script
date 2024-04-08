a32dceff9d1d9bbae207cc34cc0f4e8693112970
Tensai <kareem.tensai1@gmail.com>
2024-04-09 01:21:37 +0200

    update help message

--- clean_dir.py
+++ clean_dir.py
@@ -40,7 +40,7 @@ def help(filename):
     msg = f'''
     Usage: {filename} [OPTION] [PATH (absolute)]
 
-    Group files in PATH directory or the current directory if no PATH by OPTION
+    Group files in the specified directory (PATH) or the current directory if no directory is provided, based on the chosen grouping OPTION.
 
     Options:
         -a          Group files based on their first alphabetical letter.
